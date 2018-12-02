fed0a89a541ad0eaf6febfb002da780311316988
Michael Berber <mickberber@icloud.com>
2018-12-02 15:41:42 -0800

    complete part 1, day 2

---
+++ day2/day2_part1.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+
+var multiplesOfTwo = 0;
+var multiplesOfThree = 0;
+
+function valueCounter(acc, val, index) {
+  acc[val] = acc[val] || 0;
+  acc[val]++;
+  return acc;
+};
+
+function countIncrementer(countLib) {
+  var multiplesOfTwoCounted = false;
+  var multiplesOfThreeCounted = false;
+
+  for (var key in countLib) {
+    if (!multiplesOfTwoCounted && countLib[key] === 2) {
+      multiplesOfTwo++;
+      multiplesOfTwoCounted = true;
+    }
+
+    if (!multiplesOfThreeCounted && countLib[key] === 3) {
+      multiplesOfThree++;
+      multiplesOfThreeCounted = 3;
+    }
+  }
+};
+
+function stringSplitter(string) {
+  var stringArray = string.split('');
+  if (!stringArray.length) return;
+
+  var countLib = stringArray.reduce(valueCounter, {});
+  countIncrementer(countLib);
+};
+
+function fileReader(err, data) {
+  var dataArray = data.split('\n');
+  dataArray.forEach(stringSplitter);
+
+  console.log(multiplesOfTwo * multiplesOfThree);
+};
+
+fs.readFile('day2.txt', 'utf8', fileReader);
