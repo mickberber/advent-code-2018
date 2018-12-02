47667b79c4f20fdc4d91d7e629f7924a8cf93d3d
Michael Berber <mickberber@icloud.com>
2018-12-02 13:45:46 -0800

    refactor

--- day1/day1.js
+++ day1/day1.js
@@ -1,18 +1,16 @@
 var fs = require('fs');
 
-fs.readFile('day1.txt', 'utf8', function(err, data) {
+function fileReducer(err, data) {
   var dataArray = data.split('\n');
 
   var frequency = dataArray.reduce(function(acc, val, index) {
-    if (typeof acc === 'string') {
-      acc = parseInt(acc);
-    }
-
     var integer = parseInt(val);
-    return (typeof integer === 'number' && !isNaN(integer))
-      ? acc + integer
-      : acc;
-  });
+    return (isNaN(integer))
+      ? acc
+      : acc + integer;
+  }, 0);
 
-  console.log(frequency)
-});
+  console.log(frequency);
+}
+
+fs.readFile('day1.txt', 'utf8', fileReducer);
